82830e374c3f368b29d56163b4b34b9a1a0d398f
enfenry <enfenry@vt.edu>
2019-04-29 22:37:50 -0400

    added buttons to images that now shuffle characters upon clicking them

--- src/App.js
+++ src/App.js
@@ -6,10 +6,41 @@ import characters from "./characters.json";
 import './App.css';
 
 class App extends React.Component {
+
+  state = {
+    characters: characters
+  }
+
+  shuffleCharacters = () => {
+    const shuffledCharacters = this.shuffle(characters);
+    this.setState({shuffledCharacters});
+  }
+
+  shuffle = (array) => {
+    let currentIndex = array.length; 
+    let tempVal;
+    let randomIndex;
+  
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      tempVal = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = tempVal;
+    }
+  
+    return array;
+  }
+
+  componentDidMount = () => {
+    this.shuffleCharacters();
+  }
+
   render() {
     const mapCharacters =
       characters.map(character => {
-        return <Character name={character.name} image={character.image} key={character.name} />
+        return <Character name={character.name} image={character.image} key={character.name} shuffleCharacters = {this.shuffleCharacters} />
       })
 
     return (
--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -6,12 +6,15 @@ class Character extends Component {
     render() {
         return (
             <div className="character-card">
-                <div className="img-container">
-                {/* {console.log(this.props.image)} */}
-                    {/* <img alt={this.props.name} src={require(this.props.image)} /> */}
-                    <img alt={this.props.name} src={this.props.image} />
-                    {/* <img alt={this.props.name} src={require("../../assets/img/characters/thor.jpg")} /> */}
-                </div>
+                {/* <button id={this.props.id} className="character-button"> */}
+                <button id={this.props.id} className="character-button" onClick={() => this.props.shuffleCharacters()}>
+                    <div className="img-container">
+                        {/* {console.log(this.props.image)} */}
+                        {/* <img alt={this.props.name} src={require(this.props.image)} /> */}
+                        <img alt={this.props.name} src={this.props.image} />
+                        {/* <img alt={this.props.name} src={require("../../assets/img/characters/thor.jpg")} /> */}
+                    </div>
+                </button>
             </div>
         )
     }

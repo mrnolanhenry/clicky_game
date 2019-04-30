ea6858b74e7ecc09521e66b2a911a0bfeef54e00
enfenry <enfenry@vt.edu>
2019-04-29 22:44:16 -0400

    rewrote NavBar to accept props from App. now only App handles state

--- src/App.js
+++ src/App.js
@@ -8,46 +8,50 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    characters: characters
+    characters: characters,
+    guessMessage: "",
+    score: 0,
+    topScore: 0
   }
 
   shuffleCharacters = () => {
     const shuffledCharacters = this.shuffle(characters);
-    this.setState({shuffledCharacters});
+    this.setState({ characters: shuffledCharacters });
   }
 
   shuffle = (array) => {
-    let currentIndex = array.length; 
+    let currentIndex = array.length;
     let tempVal;
     let randomIndex;
-  
+
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       tempVal = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = tempVal;
     }
-  
+
     return array;
   }
 
   componentDidMount = () => {
     this.shuffleCharacters();
+    this.setState({ guessMessage: "Click an Image to Begin!" })
   }
 
   render() {
     const mapCharacters =
       characters.map(character => {
-        return <Character name={character.name} image={character.image} key={character.name} shuffleCharacters = {this.shuffleCharacters} />
+        return <Character name={character.name} image={character.image} key={character.name} shuffleCharacters={this.shuffleCharacters} />
       })
 
     return (
       <div className="container-fluid">
-        <NavBar /> 
+        <NavBar guessMessage={this.state.guessMessage} score={this.state.score} topScore={this.topScore} />
         <div className="row row-center">
-        <Header />
+          <Header />
         </div>
         <main className="container">
           <div className="row row-center">
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,16 +3,6 @@ import "./style.css";
 
 class NavBar extends Component {
 
-    state = {
-        guessMessage: "",
-        score: 0,
-        topScore: 0
-    }
-
-    componentDidMount() {
-        this.setState({guessMessage: "Click an Image to Begin!"})
-    }
-
     render() {
         return (
             <div className="nav-bar row">
@@ -20,10 +10,10 @@ class NavBar extends Component {
                     <a href="/"><h5>Memory Game!</h5></a>
                 </div>
                 <div className="col-sm-4 col-md-4 col-lg-4 col-center">
-                    <h5>{this.state.guessMessage}</h5>
+                    <h5>{this.props.guessMessage}</h5>
                 </div>
                 <div className="col-sm-4 col-md-4 col-lg-4 col-center">
-                    <h5>Score: {this.state.score}  | Top Score: {this.state.topScore} </h5>
+                    <h5>Score: {this.props.score}  | Top Score: {this.props.topScore} </h5>
                 </div>
             </div>
         )
